4db0a8bcc5ca49263e3b699b7dee43781efe2187
Ali Zhdanov <ali.zhdanov@kiwi.com>
2019-05-03 17:31:29 +0200

    Add create react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Wait for it!</h3>
+        <p>
+          This part under development
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
