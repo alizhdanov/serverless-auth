20fd7799a1a9eae6b17589f9fa36b8404c35eb81
Ali Zhdanov <ali.zhdanov@kiwi.com>
2019-05-04 13:48:58 +0200

    Add graphql test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4444',
+});
+
+client
+  .query({
+    query: gql`
+      {
+        instagram {
+          id
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
 
 const App: React.FC = () => {
   return (
@@ -8,12 +26,10 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Wait for it!</h3>
-        <p>
-          This part under development
-        </p>
+        <p>This part under development.</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
