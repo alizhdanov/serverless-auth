a1a87619ef0919efc376f80194a907c4432d8d5b
Ali Zhdanov <ali.zhdanov@kiwi.com>
2019-05-04 23:55:49 +0200

    Added codegen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { Query } from 'react-apollo';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,25 +10,35 @@ const client = new ApolloClient({
   uri: 'http://localhost:4444',
 });
 
-client
-  .query({
-    query: gql`
-      {
-        test
-      }
-    `,
-  })
-  .then(result => console.log(result));
+interface Data {
+  test: string;
+}
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Wait for it!</h3>
-        <p>This part under development.</p>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Wait for it!</h3>
+          <p>This part under development.</p>
+        </header>
+        <Query<Data>
+          query={gql`
+            {
+              test
+            }
+          `}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            return data && data.test;
+          }}
+        </Query>
+      </div>
+    </ApolloProvider>
   );
 };
 
--- graphql/src/utils/jwt.ts
+++ graphql/src/utils/jwt.ts
@@ -2,6 +2,7 @@ import { verify } from 'jsonwebtoken';
 
 export const getUser = (token: string) => {
   try {
+    // TODO: add public key
     // @ts-ignore
     const { data } = verify(token, 'secret');
 
